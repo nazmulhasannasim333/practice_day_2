4754890b4a0aa963f2c06c06ab81e9510897b3e0
Nasim <mdnasimhosen333@gmail.com>
2025-04-15 21:49:33 +0600

    solved problem 2

---
+++ D_Ali_Baba_and_Puzzles.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    if (a + b - c == d)
+    {
+        printf("YES\n");
+    }
+    else if (a - b + c == d)
+    {
+        printf("YES\n");
+    }
+    else if (a * b + c == d)
+    {
+        printf("YES\n");
+    }
+    else if (a + b * c == d)
+    {
+        printf("YES\n");
+    }
+    else if (a - b * c == d)
+    {
+        printf("YES\n");
+    }
+    else if (a * b - c == d)
+    {
+        printf("YES\n");
+    }
+    else
+    {
+        printf("NO\n");
+    }
+    return 0;
+}
